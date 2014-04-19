34c6037cf8b417ef1a42e90eb78cfd28f227fd39
Josh Levinger <josh@levinger.net>
2014-04-18 17:02:27 -0700

    initial token authentication

--- lib/engaging_networks/api.rb
+++ lib/engaging_networks/api.rb
@@ -1,20 +1,25 @@
+require 'engaging_networks/request/token'
+
 module EngagingNetworks
   class API < Vertebrae::API
 
     def default_options
       {
         user_agent: 'EngagingNetworksGem',
-        host: 'https://e-activist.com/',
-        prefix: 'ea-dataservice/',
+        host: 'e-activist.com',
+        prefix: '/ea-dataservice',
         content_type: 'multipart/form-data'
-
       }
     end
 
     def setup
+      puts connection.options
+
       connection.stack do |builder|
         #request middleware first, in order of importance
-        builder.use Faraday::Request::TokenAuthentication
+        builder.use EngagingNetworks::Request::TokenAuthentication,
+          :token => connection.configuration.options[:token]
+
         builder.use Faraday::Request::Multipart
 
         #response  middleware second, in reverse order of importance
