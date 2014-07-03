8a1bd846b67d9c5943e89d46727e45c41d9dab39
Josh Levinger <josh@levinger.net>
2014-07-02 18:25:57 -0700

    data.service returns only existence of object fields, not their values. need to use export.service for actual queries

--- lib/engaging_networks/supporter.rb
+++ lib/engaging_networks/supporter.rb
@@ -1,11 +1,15 @@
 module EngagingNetworks
   class Supporter < Base
-    def service
-      'SupporterData'
+
+    def exists?(email)
+      # data.service only returns Y/N values on which fields contain data, not actual data contained
+      s = client.get_request(data_path, {service: 'SupporterData', email: email, token_type: 'public'})
+      return s.obj.supporterExists == 'Y'
     end
 
-    def get(email)
-      client.get_request(data_path, {service: service, email: email, token_type: 'private'})
+    def export(startDate)
+      # startDate must be within last 45 days
+      client.get_request(export_path, {startDate: startDate, token_type: 'private'})
     end
   end
 end
